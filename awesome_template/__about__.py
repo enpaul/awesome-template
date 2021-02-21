848de7f1c6bdb36fb063369d814ed774caabed1d
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2021-02-21 14:48:05 -0500

    Update automation with latest and greatest layout
    Update tox.ini to use tox-poetry-installer
    Update about and metadata tests
    Add missing init module to tests directory

--- awesome_template/__about__.py
+++ awesome_template/__about__.py
@@ -2,5 +2,8 @@
 
 
 __title__ = "awesome-template"
+__summary__ = "An awesome python template project that does nothing"
 __version__ = "0.0.0"
-__authors__ = ["Ethan Paul <e@enp.one>"]
+__url__ = "https://github.com/enpaul/awesome-template/"
+__license__ = "MIT"
+__authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
